30e80508bb2f9d0d96f10a39332298ff54a2c755
ldeluigi <lucadelu97@gmail.com>
2022-07-04 19:26:26 +0200

    Add favicon.ico

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'spellbook',
+    'django.contrib.admin',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [

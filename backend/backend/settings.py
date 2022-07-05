f0ad84006a448deb970a6d0c14ed71247feccd75
ldeluigi <lucadelu97@gmail.com>
2022-07-05 11:08:27 +0200

    Specify logging level

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,3 +133,17 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}

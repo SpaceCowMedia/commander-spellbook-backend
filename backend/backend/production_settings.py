bb07559ea699dee9a38c90d85082f34cdbe4e99c
ldeluigi <lucadelu97@gmail.com>
2022-07-19 19:09:07 +0200

    Remove pulp

--- backend/backend/production_settings.py
+++ backend/backend/production_settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import pulp as pl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

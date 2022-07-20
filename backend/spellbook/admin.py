e29ca1615a7ab8482e8645685723e0376169dde9
ldeluigi <lucadelu97@gmail.com>
2022-07-20 13:34:48 +0200

    Fix #15

--- backend/spellbook/admin.py
+++ backend/spellbook/admin.py
@@ -81,9 +81,6 @@ class VariantAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_change_permission(self, request, obj=None):
-        return True
-
     def has_delete_permission(self, request, obj=None):
         return False
 

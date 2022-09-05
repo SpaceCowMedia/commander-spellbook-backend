36627a3a668ba785be5a46f21758fed3b5b8b7fe
ldeluigi <lucadelu97@gmail.com>
2022-09-05 20:46:32 +0200

    Reorder fields

--- backend/spellbook/models.py
+++ backend/spellbook/models.py
@@ -51,10 +51,10 @@ class Combo(models.Model):
         help_text='Features that this combo produces',
         verbose_name='produced features')
     removes = models.ManyToManyField(
-        blank=True,
         to=Feature,
         related_name='removed_by_combos',
         help_text='Features that this combo removes',
+        blank=True,
         verbose_name='removed features')
     needs = models.ManyToManyField(
         to=Feature,

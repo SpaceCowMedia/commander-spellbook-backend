534fd266c03e6b1ab39f6ca174a4f4eee46eb5de
ldeluigi <lucadelu97@gmail.com>
2022-11-11 12:04:05 +0100

    Use jquery tooltip instead of default native. Fixes #63

--- backend/spellbook/static/admin/js/card_info.js
+++ backend/spellbook/static/admin/js/card_info.js
@@ -1,6 +1,14 @@
+/**
+ * 
+ * @param {string} name 
+ * @param {HTMLElement} element 
+ */
 function fetch_card_info(name, element) {
-    if (element.title === '') {
-        element.title = name;
+    let attributeName = 'title';
+    if (typeof $ !== 'undefined' && $ !== null) {
+        attributeName = 'data-card-info';
+    }
+    if (element.getAttribute(attributeName) === null) {
         django.jQuery.ajax({
             url: 'https://api.scryfall.com/cards/named',
             dataType: 'json',
@@ -9,17 +17,25 @@ function fetch_card_info(name, element) {
                 format: 'json'
             },
             success: function(data) {
-                element.removeAttribute('title');
                 if (data.card_faces && data.card_faces.length > 0) {
-                    element.title = data.card_faces
-                        .map(e => 
+                    element.setAttribute(attributeName,
+                        data.card_faces.map(e => 
                             e.name + '  ' + e.mana_cost + ' - ' + e.type_line + '\n\n' + 
                             e.oracle_text)
-                        .join('\n🔄\n');
+                        .join('\n🔄\n'));
                 } else {
-                    element.title = 
+                    element.setAttribute(attributeName,
                         data.name + '  ' + data.mana_cost + ' - ' + data.type_line + '\n\n' +
-                        data.oracle_text;
+                        data.oracle_text);
+                }
+                if (typeof $ !== 'undefined' && $ !== null) {
+                    $(".ui-tooltip-content").parents('div').remove();
+                    $(element).tooltip({
+                        items: `[${attributeName}]`,
+                        content: function() {
+                            return this.getAttribute(attributeName);
+                        }
+                    }).mouseover();
                 }
             }
         });

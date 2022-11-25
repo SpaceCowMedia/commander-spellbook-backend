51ae81b3dd5197ec08f4f37f9f77e2729054b7af
Luca Deluigi <lucadelu97@gmail.com>
2022-11-25 15:43:08 +0100

    Optimize not_working_variants_query

--- backend/spellbook/variants/variant_data.py
+++ backend/spellbook/variants/variant_data.py
@@ -1,8 +1,16 @@
+from collections import defaultdict
 from ..models import Card, Feature, Combo, Template, Variant
 from django.db import connection
 from django.db import reset_queries
 import logging
 from django.conf import settings
+
+def fetch_not_working_variants(variants_base_query):
+    res = variants_base_query.filter(status=Variant.Status.NOT_WORKING).values('id', 'uses__id')
+    d = defaultdict(set)
+    for r in res:
+        d[r['id']].add(r['uses__id'])
+    return [frozenset(s) for s in d.values()]
 
 class Data:
     def __init__(self):
@@ -12,7 +20,7 @@ class Data:
         self.variants = Variant.objects.prefetch_related('uses', 'requires')
         self.utility_features_ids = frozenset[int](Feature.objects.filter(utility=True).values_list('id', flat=True))
         self.templates = Template.objects.prefetch_related('required_by_combos')
-        self.not_working_variants = [frozenset[int](v.uses.values_list('id', flat=True)) for v in self.variants.filter(status=Variant.Status.NOT_WORKING)] # TODO: too many queries
+        self.not_working_variants = fetch_not_working_variants(self.variants)
 
 count = 0
 def debug_queries(output=False):

f7268a88964779d76d169eebc1a1953e66cc9283
ldeluigi <lucadelu97@gmail.com>
2022-11-29 10:33:40 +0100

    Remove variant prefetch to avoid "too many sql variables" errors

--- backend/spellbook/variants/variant_data.py
+++ backend/spellbook/variants/variant_data.py
@@ -27,7 +27,7 @@ class Data:
         self.combos = Combo.objects.prefetch_related('uses', 'requires', 'needs', 'removes', 'produces')
         self.features = Feature.objects.prefetch_related('cards', 'produced_by_combos', 'needed_by_combos', 'removed_by_combos')
         self.cards = Card.objects.prefetch_related('features', 'used_in_combos')
-        self.variants = Variant.objects.prefetch_related('uses', 'requires')
+        self.variants = Variant.objects.all()
         self.utility_features_ids = frozenset[int](Feature.objects.filter(utility=True).values_list('id', flat=True))
         self.templates = Template.objects.prefetch_related('required_by_combos')
         self.not_working_variants = fetch_not_working_variants(self.variants)

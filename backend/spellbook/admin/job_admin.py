3967becdc828369085cb11ba130bf2b9c51a8cf7
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-02-21 14:52:36 +0100

    Closes #85

--- backend/spellbook/admin/job_admin.py
+++ backend/spellbook/admin/job_admin.py
@@ -5,7 +5,10 @@ from ..models import Job
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     fields = ['id', 'name', 'status', 'created', 'expected_termination', 'termination', 'message', 'started_by']
-    list_display = ['id', 'name', 'status', 'created', 'expected_termination', 'termination']
+    list_display = ['id', 'name', 'status', 'created', 'expected_termination', 'termination', 'variants_count']
+
+    def variants_count(self, obj):
+        return obj.variants.count()
 
     def has_add_permission(self, request):
         return False

e24c0ea348f95a815c86ce4a52e12e228edce69a
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-03-03 14:36:34 +0100

    Remove unused file

--- backend/spellbook/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpellbookConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spellbook'
-
-    def ready(self):
-        pass

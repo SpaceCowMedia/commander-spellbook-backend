cb9fda78feb882eb7bf5e7977ea76ea39221a5f3
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-03-09 10:43:04 +0100

    Remove demo filters for variants

--- backend/spellbook/views.py
+++ backend/spellbook/views.py
@@ -7,10 +7,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 class VariantViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Variant.objects.filter(status=Variant.Status.OK)
     serializer_class = VariantSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['unique_id', 'uses__id', 'includes__id', 'produces__id', 'of__id', 'identity']
-    search_fields = ['uses__name', 'produces__name']
-    ordering_fields = ['created', 'updated', 'unique_id']
+    # filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    # filterset_fields = ['unique_id', 'uses__id', 'includes__id', 'produces__id', 'of__id', 'identity']
+    # search_fields = ['uses__name', 'produces__name']
+    # ordering_fields = ['created', 'updated', 'unique_id']
 
 
 variant_list = VariantViewSet.as_view({'get': 'list'})

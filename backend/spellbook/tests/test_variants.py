bf8df0074fa5ebd80df77304aff0dedfda05e425
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-04-17 14:51:44 +0200

    Add variant set tests

--- backend/spellbook/tests/test_variants.py
+++ backend/spellbook/tests/test_variants.py
@@ -217,6 +217,13 @@ class VariantSetTests(TestCase):
         self.assertEqual(variant_set.key_to_ingredients(variant_set.ingredients_to_key([], [1, 2, 3])), ([], [1, 2, 3]))
         self.assertEqual(variant_set.key_to_ingredients(variant_set.ingredients_to_key([], [])), ([], []))
         self.assertEqual(variant_set.key_to_ingredients(variant_set.ingredients_to_key([1], [1])), ([1], [1]))
+    
+    def test_key_to_ingredients(self):
+        variant_set = VariantSet()
+        self.assertEqual(variant_set.ingredients_to_key(*variant_set.key_to_ingredients(frozenset())), frozenset())
+        self.assertEqual(variant_set.ingredients_to_key(*variant_set.key_to_ingredients(frozenset({'C1', 'C2', 'T1'}))), frozenset({'C1', 'C2', 'T1'}))
+        self.assertEqual(variant_set.ingredients_to_key(*variant_set.key_to_ingredients(frozenset({'C1', 'C2', 'T1', 'T2'}))), frozenset({'C1', 'C2', 'T1', 'T2'}))
+        self.assertEqual(variant_set.ingredients_to_key(*variant_set.key_to_ingredients(frozenset({'C1', 'C2', 'T1', 'T3'}))), frozenset({'C1', 'C2', 'T1', 'T3'}))
 
     def test_variant_set_add(self):
         variant_set = VariantSet()
@@ -237,6 +244,29 @@ class VariantSetTests(TestCase):
         variant_set.add([2, 4], [])
         self.assertEqual(list_of_tuples_of_lists_to_set(variant_set.variants()), {((2, 4), ())})
 
+    def test_is_satisfied_by(self):
+        variant_set = VariantSet()
+        variant_set.add([1, 2, 3, 4], [1, 2, 3])
+        variant_set.add([1, 2, 3, 4, 5], [1, 2])
+        self.assertTrue(variant_set.is_satisfied_by([1, 2, 3, 4], [1, 2, 3]))
+        self.assertTrue(variant_set.is_satisfied_by([1, 2, 3, 4, 5], [1, 2]))
+        self.assertFalse(variant_set.is_satisfied_by([1, 2, 3, 4], [1, 2]))
+        self.assertTrue(variant_set.is_satisfied_by([1, 2, 3, 4, 5], [1, 2, 3]))
+        self.assertTrue(variant_set.is_satisfied_by([0, 1, 2, 3, 4, 5], [1, 2, 3, 4]))
+        self.assertFalse(variant_set.is_satisfied_by([], []))
+
+    def test_len(self):
+        variant_set = VariantSet()
+        self.assertEqual(len(variant_set), 0)
+        variant_set.add([1, 2, 3, 4], [1, 2, 3])
+        self.assertEqual(len(variant_set), 1)
+        variant_set.add([1, 2, 3, 4, 5], [1, 2])
+        self.assertEqual(len(variant_set), 2)
+        variant_set.add([1, 2, 3, 4], [1, 2, 3])
+        self.assertEqual(len(variant_set), 2)
+        variant_set.add([1, 2, 3, 4], [1, 2])
+        self.assertEqual(len(variant_set), 1)
+
     def test_variant_set_or(self):
         variant_set = VariantSet()
         variant_set.add([1, 2, 3, 4], [1, 2, 3])
@@ -249,11 +279,18 @@ class VariantSetTests(TestCase):
         self.assertSetEqual(list_of_tuples_of_lists_to_set(variant_set3.variants()), list_of_tuples_of_lists_to_set([([1, 2, 3, 4], [1, 2, 3]), ([1, 2, 3, 4, 5], [1]), ([1, 2, 3], [1, 2, 3, 4])]))
         variant_set4 = variant_set2 | variant_set
         self.assertSetEqual(list_of_tuples_of_lists_to_set(variant_set3.variants()), list_of_tuples_of_lists_to_set(variant_set4.variants()))
+    
+        variant_set2.add([2, 3], [2])
+        variant_set3 = variant_set | variant_set2
+        self.assertSetEqual(list_of_tuples_of_lists_to_set(variant_set3.variants()), list_of_tuples_of_lists_to_set([([2, 3], [2]), ([1, 2, 3, 4, 5], [1])]))
 
         self.assertIsNotNone(VariantSet.or_sets([]))
         variant_set5 = VariantSet.or_sets([variant_set])
         self.assertIsNotNone(variant_set5)
         self.assertFalse(variant_set5 is variant_set)
+
+        self.assertEqual((variant_set | variant_set2).variants(), (variant_set2 + variant_set).variants())
+        self.assertEqual(VariantSet.or_sets([variant_set, variant_set2, variant_set3]).variants(), (variant_set2 + variant_set + variant_set3).variants())
 
     def test_variant_set_and(self):
         variant_set = VariantSet()
@@ -268,7 +305,25 @@ class VariantSetTests(TestCase):
         variant_set4 = variant_set2 & variant_set
         self.assertSetEqual(list_of_tuples_of_lists_to_set(variant_set3.variants()), list_of_tuples_of_lists_to_set(variant_set4.variants()))
 
+        variant_set2.add([2, 3], [2])
+        variant_set3 = variant_set & variant_set2
+        self.assertSetEqual(list_of_tuples_of_lists_to_set(variant_set3.variants()), list_of_tuples_of_lists_to_set([([1, 2, 3, 4], [1, 2, 3]), ([1, 2, 3, 4, 5], [1, 2])]))
+
         self.assertIsNotNone(VariantSet.and_sets([]))
         variant_set5 = VariantSet.and_sets([variant_set])
         self.assertIsNotNone(variant_set5)
         self.assertFalse(variant_set5 is variant_set)
+
+        self.assertEqual((variant_set & variant_set2).variants(), (variant_set2 * variant_set).variants())
+        self.assertEqual(VariantSet.and_sets([variant_set, variant_set2, variant_set3]).variants(), (variant_set2 * variant_set * variant_set3).variants())
+
+    def test_copy(self):
+        variant_set = VariantSet()
+        variant_set.add([1, 2, 3, 4], [1, 2, 3])
+        variant_set.add([1, 2, 3, 4, 5], [1, 2])
+        variant_set2 = variant_set.copy()
+        self.assertIsNotNone(variant_set2)
+        self.assertFalse(variant_set2 is variant_set)
+        self.assertEqual(variant_set2.variants(), variant_set.variants())
+        variant_set2.add([1, 2], [1, 2])
+        self.assertNotEqual(variant_set2.variants(), variant_set.variants())

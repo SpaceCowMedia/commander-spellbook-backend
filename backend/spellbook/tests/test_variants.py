908267d6f1499bd3f48048aa3eefc3cf0757e2a4
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-04-17 12:00:19 +0200

    Add variant data tests

--- backend/spellbook/tests/test_variants.py
+++ backend/spellbook/tests/test_variants.py
@@ -1,8 +1,12 @@
-from unittest import skip
 from django.test import TestCase
 from spellbook.variants.minimal_set_of_sets import MinimalSetOfSets
 from spellbook.variants.variant_set import VariantSet
+from spellbook.variants.variants_generator import id_from_cards_and_templates_ids
 from spellbook.variants.list_utils import merge_identities, includes_any
+from spellbook.variants.variant_data import Data, debug_queries
+from spellbook.utils import launch_job_command
+from spellbook.models import Variant, Combo, Feature, Card, Template
+from .abstract_test import AbstractModelTests
 
 
 def list_of_tuples_of_lists_to_set(list_of_tuples_of_lists: list[tuple[list]]) -> set[tuple[tuple]]:
@@ -136,6 +140,74 @@ class MinimalSetOfSetsTests(TestCase):
         self.assertNotEqual(self.aset, c)
         self.assertNotEqual(len(self.aset), len(c))
 
+
+class VariantDataTests(AbstractModelTests):
+    def test_combos(self):
+        data = Data()
+        self.assertEqual(len(data.combos), Combo.objects.count())
+        self.assertDictEqual({k: data.combo_to_cards[k] for k in Combo.objects.values_list('id', flat=True)}, {combo.id: list(combo.cardincombo_set.all()) for combo in Combo.objects.all()})
+        self.assertDictEqual({k: data.combo_to_templates[k] for k in Combo.objects.values_list('id', flat=True)}, {combo.id: list(combo.templateincombo_set.all()) for combo in Combo.objects.all()})
+        self.assertEqual(set(c.id for c in data.generator_combos), set(Combo.objects.filter(generator=True).values_list('id', flat=True)))
+
+    def test_features(self):
+        data = Data()
+        self.assertEqual(set(data.features.values_list('id', flat=True)), set(Feature.objects.values_list('id', flat=True)))
+
+    def test_cards(self):
+        data = Data()
+        self.assertEqual(set(data.cards.values_list('id', flat=True)), set(Card.objects.values_list('id', flat=True)))
+
+    def test_variants(self):
+        data = Data()
+        self.assertEqual(set(data.variants.values_list('id', flat=True)), set(Variant.objects.values_list('id', flat=True)))
+
+    def test_templates(self):
+        data = Data()
+        self.assertEqual(set(data.templates.values_list('id', flat=True)), set(Template.objects.values_list('id', flat=True)))
+
+    def test_utility_features_ids(self):
+        data = Data()
+        self.assertSetEqual(data.utility_features_ids, set(Feature.objects.filter(utility=True).values_list('id', flat=True)))
+
+    def test_not_working_variants(self):
+        launch_job_command('generate_variants', None)
+        self.v1_id = id_from_cards_and_templates_ids([self.c8_id, self.c1_id], [self.t1_id])
+        v1 = Variant.objects.get(id=self.v1_id)
+        v1.status = Variant.Status.NOT_WORKING
+        v1.save()
+        data = Data()
+        self.assertEqual(data.not_working_variants, [frozenset({self.c8_id, self.c1_id})])
+        launch_job_command('generate_variants', None)
+        data = Data()
+        self.assertEqual(data.not_working_variants, [frozenset({self.c8_id, self.c1_id})] * 2)
+
+    def test_id_to_x(self):
+        data = Data()
+        self.assertEqual(data.id_to_variant, {v.id: v for v in Variant.objects.all()})
+        self.assertEqual(data.id_to_combo, {c.id: c for c in Combo.objects.all()})
+        self.assertEqual(data.id_to_card, {c.id: c for c in Card.objects.all()})
+        self.assertEqual(data.id_to_template, {t.id: t for t in Template.objects.all()})
+
+    def test_card_in_variant(self):
+        data = Data()
+        for card_id, variant_id in data.card_in_variant:
+            self.assertIn(card_id, set(data.id_to_variant[variant_id].uses.all().values_list('id', flat=True)))
+
+    def test_template_in_variant(self):
+        data = Data()
+        for template_id, variant_id in data.template_in_variant:
+            self.assertIn(template_id, set(data.id_to_variant[variant_id].uses.all().values_list('id', flat=True)))
+
+    def test_combo_to_removed_features(self):
+        data = Data()
+        for combo_id, removed_feature_ids in data.combo_to_removed_features.items():
+            self.assertEqual(removed_feature_ids, set(data.id_to_combo[combo_id].removes.all().values_list('id', flat=True)))
+
+    def test_debug_queries(self):
+        with self.settings(DEBUG=True):
+            q = debug_queries()
+            Variant.objects.all().count()
+            self.assertEqual(debug_queries() - q, 1)
 
 class VariantSetTests(TestCase):
     def test_ingredients_to_key(self):
--- backend/spellbook/variants/variant_data.py
+++ backend/spellbook/variants/variant_data.py
@@ -9,22 +9,6 @@ from spellbook.models.template import Template
 from spellbook.models.variant import Variant, CardInVariant, TemplateInVariant
 
 
-def fetch_not_working_variants(variants_base_query):
-    res = variants_base_query.filter(status=Variant.Status.NOT_WORKING).values('id', 'uses__id')
-    d = defaultdict[str, set[int]](set)
-    for r in res:
-        d[r['id']].add(r['uses__id'])
-    return [frozenset(s) for s in d.values()]
-
-
-def fetch_removed_features(combos_base_query):
-    res = combos_base_query.values('id', 'removes__id')
-    d = defaultdict[int, set[int]](set)
-    for r in res:
-        d[r['id']].add(r['removes__id'])
-    return d
-
-
 class RestoreData:
     def __init__(self):
         self.combos = Combo.objects.prefetch_related('uses', 'requires', 'needs', 'removes', 'produces')
@@ -39,20 +23,35 @@ class RestoreData:
 
 class Data(RestoreData):
     def __init__(self):
+        def fetch_not_working_variants(variants_base_query):
+            res = variants_base_query.filter(status=Variant.Status.NOT_WORKING).values('id', 'uses__id')
+            d = defaultdict[str, set[int]](set)
+            for r in res:
+                d[r['id']].add(r['uses__id'])
+            return [frozenset(s) for s in d.values()]
+
+        def fetch_removed_features(combos_base_query):
+            res = combos_base_query.values('id', 'removes__id')
+            d = defaultdict[int, set[int]](set)
+            for r in res:
+                if r['removes__id'] is not None:
+                    d[r['id']].add(r['removes__id'])
+            return d
+
         super().__init__()
         self.features = Feature.objects.prefetch_related('cards', 'produced_by_combos', 'needed_by_combos', 'removed_by_combos')
         self.cards = Card.objects.prefetch_related('features', 'used_in_combos')
         self.variants = Variant.objects.all()
-        self.utility_features_ids = frozenset[int](Feature.objects.filter(utility=True).values_list('id', flat=True))
         self.templates = Template.objects.prefetch_related('required_by_combos')
+        self.utility_features_ids = frozenset[int](Feature.objects.filter(utility=True).values_list('id', flat=True))
         self.not_working_variants = fetch_not_working_variants(self.variants)
         self.id_to_variant = {v.id: v for v in self.variants}
-        self.combo_to_removed_features = fetch_removed_features(self.combos)
         self.id_to_combo: dict[int, Combo] = {c.id: c for c in self.combos}
         self.id_to_card: dict[int, Card] = {c.id: c for c in self.cards}
         self.id_to_template: dict[int, Template] = {t.id: t for t in self.templates}
         self.card_in_variant = {(civ.card.id, civ.variant.id): civ for civ in CardInVariant.objects.select_related('card', 'variant')}
         self.template_in_variant = {(tiv.template.id, tiv.variant.id): tiv for tiv in TemplateInVariant.objects.select_related('template', 'variant')}
+        self.combo_to_removed_features = fetch_removed_features(self.combos)
 
 
 count = 0

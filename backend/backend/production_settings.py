b5603889ed33cb3288676842dafef245bfeedc5f
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-05-03 14:03:42 +0200

    Disable server side cursors

--- backend/backend/production_settings.py
+++ backend/backend/production_settings.py
@@ -107,9 +107,9 @@ DATABASES = {
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
-        "OPTIONS": {
-            "server_side_binding": True,
-        } if 'postgres' in os.environ.get("SQL_ENGINE", '') else {},
+        # "OPTIONS": {
+        #     "server_side_binding": True,
+        # } if 'postgres' in os.environ.get("SQL_ENGINE", '') else {},
     }
 }
 

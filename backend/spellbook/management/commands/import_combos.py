c7ebb16ed90415fec6bc722c7e95a3d3024987df
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-06-03 20:41:44 +0200

    Fix and improve card ordering

--- backend/spellbook/management/commands/import_combos.py
+++ backend/spellbook/management/commands/import_combos.py
@@ -30,33 +30,49 @@ def sorted_prereq_search_terms(prereq: str, card_set: set[str]):
     found_terms = list[tuple[str, int]]()
     for term in terms:
         lower_term = term.lower()
-        if lower_term in pre_lower:
-            found_terms.append((term, pre_lower.index(lower_term)))
+        regex = rf'(?:^|[^\w])({re.escape(lower_term)})(?:[^\w]|$)'
+        search_result = re.search(regex, pre_lower)
+        if search_result:
+            found_terms.append((term, search_result.start(1)))
             continue
         if '//' in term:
             complete_lower_term = lower_term
-            lower_term = complete_lower_term.partition('//')[0].strip() + ' '
-            if lower_term in pre_lower:
-                found_terms.append((term, pre_lower.index(lower_term)))
-                continue
+            split_over_slashes = complete_lower_term.partition('//')
+            lower_term = split_over_slashes[0].strip()
+            regex = rf'(?:^|[^\w])({re.escape(lower_term)})(?:[^\w]|$)'
+            search_result = re.search(regex, pre_lower)
+            first_occurrence = None
+            if search_result:
+                first_occurrence = search_result.start(1)
             if ',' in lower_term:
-                lower_term = lower_term.partition(',')[0] + ' '
-                if lower_term in pre_lower:
-                    found_terms.append((term, pre_lower.index(lower_term)))
-                    continue
-            lower_term = complete_lower_term.partition('//')[2].strip() + ' '
-            if lower_term in pre_lower:
-                found_terms.append((term, pre_lower.index(lower_term)))
-                continue
+                lower_term = lower_term.partition(',')[0]
+                regex = rf'(?:^|[^\w])({re.escape(lower_term)})(?:[^\w]|$)'
+                search_result = re.search(regex, pre_lower)
+                if search_result:
+                    if first_occurrence is None or search_result.start(1) < first_occurrence:
+                        first_occurrence = search_result.start(1)
+            lower_term = split_over_slashes[2].strip()
+            regex = rf'(?:^|[^\w])({re.escape(lower_term)})(?:[^\w]|$)'
+            search_result = re.search(regex, pre_lower)
+            if search_result:
+                if first_occurrence is None or search_result.start(1) < first_occurrence:
+                    first_occurrence = search_result.start(1)
             if ',' in lower_term:
-                lower_term = lower_term.partition(',')[0] + ' '
-                if lower_term in pre_lower:
-                    found_terms.append((term, pre_lower.index(lower_term)))
-                    continue
+                lower_term = lower_term.partition(',')[0]
+                regex = rf'(?:^|[^\w])({re.escape(lower_term)})(?:[^\w]|$)'
+                search_result = re.search(regex, pre_lower)
+                if search_result:
+                    if first_occurrence is None or search_result.start(1) < first_occurrence:
+                        first_occurrence = search_result.start(1)
+            if first_occurrence is not None:
+                found_terms.append((term, first_occurrence))
+                continue
         elif ',' in term:
-            lower_term = lower_term.partition(',')[0] + ' '
-            if lower_term in pre_lower:
-                found_terms.append((term, pre_lower.index(lower_term)))
+            lower_term = lower_term.partition(',')[0]
+            regex = rf'(?:^|[^\w])({re.escape(lower_term)})(?:[^\w]|$)'
+            search_result = re.search(regex, pre_lower)
+            if search_result:
+                found_terms.append((term, search_result.start(1)))
                 continue
     return [item[0] for item in sorted(found_terms, key=lambda item: item[1])]
 

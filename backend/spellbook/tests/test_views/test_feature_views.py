3d11487592d45e92faba768a495e1420ec92f4ce
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-11-06 16:01:51 +0100

    Fix tests. Closes #221

--- backend/spellbook/tests/test_views/test_feature_views.py
+++ backend/spellbook/tests/test_views/test_feature_views.py
@@ -19,16 +19,16 @@ class FeatureViewsTests(AbstractModelTests):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('Content-Type'), 'application/json')
         result = json.loads(response.content, object_hook=json_to_python_lambda)
-        features_count = Feature.objects.count()
+        features_count = Feature.objects.filter(utility=False).count()
         self.assertEqual(len(result.results), features_count)
         for i in range(features_count):
             self.feature_assertions(result.results[i])
 
     def test_features_detail_view(self):
         c = Client()
-        response = c.get('/features/{}'.format(self.f1_id), follow=True)
+        response = c.get('/features/{}'.format(self.f2_id), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('Content-Type'), 'application/json')
         result = json.loads(response.content, object_hook=json_to_python_lambda)
-        self.assertEqual(result.id, self.f1_id)
+        self.assertEqual(result.id, self.f2_id)
         self.feature_assertions(result)

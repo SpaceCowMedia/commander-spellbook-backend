19b99bae9acb42998195018c2f68d558757b2451
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-12-16 17:27:58 +0100

    Fix edhrec update script to support new variant ids

--- backend/spellbook/management/edhrec.py
+++ backend/spellbook/management/edhrec.py
@@ -18,20 +18,18 @@ def edhrec():
     variants_db = dict[str, dict]()
     with urlopen(req) as response:
         data = json.loads(response.read().decode())
-        for old_variant_id, variant_data in data['combos'].items():
-            if old_variant_id not in variants_id_map:
-                continue
-            variant_id = variants_id_map[old_variant_id]
+        for variant_id, variant_data in data['combos'].items():
+            if variant_id in variants_id_map:
+                variant_id = variants_id_map[variant_id]
             if variant_id not in variants_db:
                 variants_db[variant_id] = {
                     'popularity': variant_data['count'],
                 }
             else:
                 raise Exception(f'Variant {variant_id} has multiple entries in EDHREC data')
-        for old_variant_id in data['errors'].keys():
-            if old_variant_id not in variants_id_map:
-                continue
-            variant_id = variants_id_map[old_variant_id]
+        for variant_id in data['errors'].keys():
+            if variant_id in variants_id_map:
+                variant_id = variants_id_map[variant_id]
             if variant_id not in variants_db:
                 variants_db[variant_id] = {
                     'popularity': 0,

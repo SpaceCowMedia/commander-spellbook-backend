a25afce04e5ad6602e6c293dbd11c573d08c93c4
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-01-19 14:21:39 +0100

    Improve feature admin search results. Closes #309

--- backend/spellbook/admin/feature_admin.py
+++ backend/spellbook/admin/feature_admin.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models import QuerySet, Case, When
+from django.http import HttpRequest
 from spellbook.models import Feature
 from .utils import SpellbookModelAdmin
 
@@ -25,3 +28,13 @@ class FeatureAdmin(SpellbookModelAdmin):
         return super().lookup_allowed(lookup, value) or lookup in (
             'produced_by_variants__id',
         )
+
+    def get_search_results(self, request: HttpRequest, queryset: QuerySet[Any], search_term: str) -> tuple[QuerySet[Any], bool]:
+        queryset, duplicates = super().get_search_results(request, queryset, search_term)
+        return queryset.alias(
+            match_points=Case(
+                When(name__iexact=search_term, then=5),
+                When(name__istartswith=search_term, then=4),
+                default=1,
+            )
+        ).order_by('-match_points', 'name'), duplicates

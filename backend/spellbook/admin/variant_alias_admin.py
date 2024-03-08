54075cfaad9153872c2e4e07e8a0784249c5dcfb
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-03-08 16:36:34 +0100

    Add empty/nonempty filter to variant alias admin. Closes #341

--- backend/spellbook/admin/variant_alias_admin.py
+++ backend/spellbook/admin/variant_alias_admin.py
@@ -8,3 +8,6 @@ class VariantAliasAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'variant', 'description']
     search_fields = ['id', 'variant__id', 'description']
     raw_id_fields = ['variant']
+    list_filter = [
+        ('variant', admin.EmptyFieldListFilter)
+    ]

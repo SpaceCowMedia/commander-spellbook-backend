40b6291ddf8ede682872ef4b2c634ccd78b5c9b9
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-05-04 17:21:17 +0200

    Refactor list utils for multisets

--- backend/spellbook/tests/test_variants_generation/test_list_utils.py
+++ backend/spellbook/tests/test_variants_generation/test_list_utils.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from spellbook.variants.list_utils import includes_any
+from multiset import FrozenMultiset
 
 
 class ListUtilsTests(TestCase):
     def test_includes_any(self):
-        self.assertTrue(includes_any({1, 2, 3}, [{1, 2, 3}]))
-        self.assertTrue(includes_any({1, 2, 3}, [{1, 2, 3}, {1, 2, 3, 4}]))
-        self.assertTrue(includes_any({1, 2, 3}, [{1, 2, 3, 4}, {2}]))
-        self.assertTrue(includes_any(set(), [set()]))
-        self.assertTrue(includes_any({1}, [{2}, {1}]))
-        self.assertFalse(includes_any({1, 2, 3}, [{1, 4}, {4, 3}]))
-        self.assertFalse(includes_any(set(), [{1, 2, 3}]))
+        self.assertTrue(includes_any(FrozenMultiset([1, 2, 3]), [FrozenMultiset([1, 2, 3])]))
+        self.assertTrue(includes_any(FrozenMultiset([1, 2, 3]), [FrozenMultiset([1, 2, 3]), FrozenMultiset([1, 2, 3, 4])]))
+        self.assertTrue(includes_any(FrozenMultiset([1, 2, 3]), [FrozenMultiset([1, 2, 3, 4]), FrozenMultiset([2])]))
+        self.assertTrue(includes_any(FrozenMultiset(), [FrozenMultiset()]))
+        self.assertTrue(includes_any(FrozenMultiset([1]), [FrozenMultiset([2]), FrozenMultiset([1])]))
+        self.assertFalse(includes_any(FrozenMultiset([1, 2, 3]), [FrozenMultiset([1, 4]), FrozenMultiset([4, 3])]))
+        self.assertFalse(includes_any(FrozenMultiset(), [FrozenMultiset([1, 2, 3])]))
--- backend/spellbook/variants/list_utils.py
+++ backend/spellbook/variants/list_utils.py
@@ -1,8 +1,9 @@
-from typing import TypeVar, Iterable
+from typing import TypeVar, Iterable, Set
+from multiset import BaseMultiset
 
 
 T = TypeVar('T')
 
 
-def includes_any(v: frozenset[T] | set[T], others: Iterable[frozenset[T] | set[T]]) -> bool:
+def includes_any(v: BaseMultiset[T], others: Iterable[BaseMultiset[T] | Set[T]]) -> bool:
     return any(v.issuperset(o) for o in others)

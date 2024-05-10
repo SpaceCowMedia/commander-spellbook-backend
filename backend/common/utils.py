c37f15aadd5c67e0a60603ee9237e59b10366894
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-05-10 16:13:46 +0200

    Fix bug for missing pypy

--- backend/common/utils.py
+++ backend/common/utils.py
@@ -2,10 +2,13 @@ import subprocess
 
 
 def check_pypy():
-    result = subprocess.run(['pypy', '--version'], stdout=subprocess.PIPE)
-    if result.returncode != 0:
+    try:
+        result = subprocess.run(['pypy', '--version'], stdout=subprocess.PIPE)
+        if result.returncode != 0:
+            return False
+        return result.stdout.decode().startswith('Python 3.')
+    except FileNotFoundError:
         return False
-    return result.stdout.decode().startswith('Python 3.')
 
 
 PYPY_AVAILABLE = check_pypy()

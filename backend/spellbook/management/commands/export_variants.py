7d14a7d67f9b796d79964414570412e411ab29ed
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-05-31 19:34:07 +0200

    Add aliases to bulk json

--- backend/spellbook/management/commands/export_variants.py
+++ backend/spellbook/management/commands/export_variants.py
@@ -7,9 +7,10 @@ from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 from django.db import transaction
 from djangorestframework_camel_case.util import camelize
-from spellbook.models import Variant, Job
-from spellbook.serializers import VariantSerializer
+from spellbook.models import Variant, Job, VariantAlias
+from spellbook.serializers import VariantSerializer, VariantAliasSerializer
 from spellbook.views.variants import VariantViewSet
+from spellbook.views.variant_aliases import VariantAliasViewSet
 from ..abstract_command import AbstractCommand
 from ..s3_upload import upload_json_to_aws
 
@@ -18,6 +19,10 @@ DEFAULT_VARIANTS_FILE_NAME = 'variants.json'
 
 def prepare_variant(variant: Variant):
     return VariantViewSet.serializer_class(variant).data
+
+
+def prepare_variant_alias(variant_alias: VariantAlias):
+    return VariantAliasViewSet.serializer_class(variant_alias).data
 
 
 class Command(AbstractCommand):
@@ -46,11 +51,15 @@ class Command(AbstractCommand):
         self.log('Updating variants preserialized representation...')
         Variant.objects.bulk_serialize(objs=variants_source, serializer=VariantSerializer, batch_size=5000)  # type: ignore
         self.log('Updating variants preserialized representation...done', self.style.SUCCESS)
+        self.log('Fetching variant aliases from db...')
+        with transaction.atomic(durable=True):
+            variants_alias_source = list[VariantAlias](VariantAliasSerializer.prefetch_related(VariantAliasViewSet.queryset))
         self.log('Exporting variants...')
         result = {
             'timestamp': timezone.now().isoformat(),
             'version': settings.VERSION,
             'variants': [prepare_variant(v) for v in variants_source if v.status in Variant.public_statuses()],
+            'aliases': [prepare_variant_alias(va) for va in variants_alias_source],
         }
         camelized_json = camelize(result)
         if options['s3']:

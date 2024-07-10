9f7cca45442daf9225cbb0c431d3e8d302c44d25
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-07-10 10:29:51 +0200

    Add "used in" section under feature attributes change form

--- backend/spellbook/admin/feature_attribute_admin.py
+++ backend/spellbook/admin/feature_attribute_admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from spellbook.models import FeatureAttribute
-from .utils import SpellbookModelAdmin
+from django.db.models import Q
+from spellbook.models import FeatureAttribute, Combo
+from .utils import SpellbookModelAdmin, SpellbookAdminForm
+
+
+class FeatureAttributeForm(SpellbookAdminForm):
+    def used_in_combos(self):
+        if self.instance.pk is None:
+            return Combo.objects.none()
+        return Combo.objects.filter(
+            Q(featureneededincombo__any_of_attributes=self.instance) | Q(featureneededincombo__all_of_attributes=self.instance) | Q(featureneededincombo__none_of_attributes=self.instance) | Q(featureproducedincombo__attributes=self.instance)
+        ).order_by('-created')
 
 
 @admin.register(FeatureAttribute)
 class FeatureAttributeAdmin(SpellbookModelAdmin):
+    form = FeatureAttributeForm
     readonly_fields = ['id']
     fields = ['id', 'name']
     search_fields = ['name']

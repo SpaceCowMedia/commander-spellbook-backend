3e31a47113815de71c9b958b11583c5f8f005fa1
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-07-20 17:54:30 +0200

    Attempt to fix code for older python versions

--- backend/spellbook/parsers/variants_query_grammar.py
+++ backend/spellbook/parsers/variants_query_grammar.py
@@ -4,5 +4,5 @@ from spellbook.models import Variant
 
 with open(Path(__file__).parent / 'variants_query_grammar.lark', 'r') as f:
     VARIANTS_QUERY_GRAMMAR = f.read() + f'''
-        SUPPORTED_STORE : {' | '.join(f'"{s.removeprefix('price_')}"i' for s in Variant.prices_fields())}
+        SUPPORTED_STORE : {' | '.join(f'"{s.removeprefix("price_")}"i' for s in Variant.prices_fields())}
     '''

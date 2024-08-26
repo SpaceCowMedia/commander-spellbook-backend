69e6c287b762cd89a36d8e9ef286f5dad66e9fc4
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-08-26 21:20:27 +0200

    Fix generate client script

--- client/generate-client.sh
+++ client/generate-client.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-VERSION=`python ../backend/manage.py shell -c 'from django.conf import settings; print(settings.VERSION)'`
+
 MSYS_NO_PATHCONV=1 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -i /local/openapi.yaml \
   -g python \
   -o /local/python \
-  --additional-properties=generateSourceCodeOnly=true,library=asyncio,packageName=spellbook_client,packageVersion=${VERSION}
+  --additional-properties=generateSourceCodeOnly=true,library=asyncio,packageName=spellbook_client

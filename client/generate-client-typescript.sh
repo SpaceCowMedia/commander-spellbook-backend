fa7572d1159f68ae1149a0ee0b8594d9313050c6
Luca Deluigi <lucadelu97@gmail.com>
2024-08-30 09:29:35 +0200

    Add typescript client generation

---
+++ client/generate-client-typescript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+MSYS_NO_PATHCONV=1 docker run --rm \
+  -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+  -i /local/openapi.yaml \
+  -g typescript-fetch \
+  -o /local/typescript/spellbook_client \
+  --additional-properties=fileNaming=camelCase,stringEnums=true

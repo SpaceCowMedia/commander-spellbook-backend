e44f2bba17bd5744bdf103f8fda9450e37b5956b
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-09-02 14:29:44 +0200

    Publish typescript client as

--- client/generate-client-typescript.sh
+++ client/generate-client-typescript.sh
@@ -5,5 +5,5 @@ MSYS_NO_PATHCONV=1 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -i /local/openapi.yaml \
   -g typescript-fetch \
-  -o /local/typescript/spellbook_client \
+  -o /local/typescript/spellbook-client \
   --additional-properties=fileNaming=camelCase,stringEnums=true

189d726550ed0aa91094cc4042867006ea350d9a
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2025-01-02 11:46:36 +0100

    Fix #685

--- backend/spellbook/variants/variant_data.py
+++ backend/spellbook/variants/variant_data.py
@@ -61,34 +61,50 @@ class Data:
         self.generator_combos = [c for c in combos if c.status == Combo.Status.GENERATOR]
         self.combo_to_cards = {c.id: list[CardInCombo]() for c in combos}
         for cardincombo in cardincombos:
-            self.combo_to_cards[cardincombo.combo_id].append(cardincombo)
+            x = self.combo_to_cards.get(cardincombo.combo_id)
+            if x is not None:
+                x.append(cardincombo)
         for i in self.combo_to_cards.values():
             i.sort(key=lambda cic: cic.order)
         self.combo_to_templates = {c.id: list[TemplateInCombo]() for c in combos}
         for i in templateincombos:
-            self.combo_to_templates[i.combo_id].append(i)
+            x = self.combo_to_templates.get(i.combo_id)
+            if x is not None:
+                x.append(i)
         for i in self.combo_to_templates.values():
             i.sort(key=lambda tic: tic.order)
         self.combo_to_produced_features = {c.id: list[FeatureProducedInCombo]() for c in combos}
         for i in featureproducedincombos:
-            self.combo_to_produced_features[i.combo_id].append(i)
+            x = self.combo_to_produced_features.get(i.combo_id)
+            if x is not None:
+                x.append(i)
         self.combo_to_needed_features = {c.id: list[FeatureNeededInCombo]() for c in combos}
         for i in featureneededincombos:
-            self.combo_to_needed_features[i.combo_id].append(i)
+            x = self.combo_to_needed_features.get(i.combo_id)
+            if x is not None:
+                x.append(i)
         self.combo_to_removed_features = {c.id: list[FeatureRemovedInCombo]() for c in combos}
         for i in featureremovedincombos:
-            self.combo_to_removed_features[i.combo_id].append(i)
+            x = self.combo_to_removed_features.get(i.combo_id)
+            if x is not None:
+                x.append(i)
 
         self.feature_needed_in_combo_to_attributes_matcher = dict[int, AttributesMatcher]()
         feature_needed_in_combo_to_any_of_attributes = {f.id: set[int]() for f in featureneededincombos}
         for i in featureneededincombo_anyofattributes:
-            feature_needed_in_combo_to_any_of_attributes[i.featureneededincombo_id].add(i.featureattribute_id)
+            x = feature_needed_in_combo_to_any_of_attributes.get(i.featureneededincombo_id)
+            if x is not None:
+                x.add(i.featureattribute_id)
         feature_needed_in_combo_to_all_of_attributes = {f.id: set[int]() for f in featureneededincombos}
         for i in featureneededincombo_allofattributes:
-            feature_needed_in_combo_to_all_of_attributes[i.featureneededincombo_id].add(i.featureattribute_id)
+            x = feature_needed_in_combo_to_all_of_attributes.get(i.featureneededincombo_id)
+            if x is not None:
+                x.add(i.featureattribute_id)
         feature_needed_in_combo_to_none_of_attributes = {f.id: set[int]() for f in featureneededincombos}
         for i in featureneededincombo_noneofattributes:
-            feature_needed_in_combo_to_none_of_attributes[i.featureneededincombo_id].add(i.featureattribute_id)
+            x = feature_needed_in_combo_to_none_of_attributes.get(i.featureneededincombo_id)
+            if x is not None:
+                x.add(i.featureattribute_id)
         for i in featureneededincombos:
             self.feature_needed_in_combo_to_attributes_matcher[i.id] = AttributesMatcher(
                 any_of=frozenset(feature_needed_in_combo_to_any_of_attributes[i.id]),
@@ -97,40 +113,57 @@ class Data:
             )
         self.feature_produced_in_combo_to_attributes = {f.id: set[int]() for f in featureproducedincombos}
         for i in featureproducedincombo_attributes:
-            self.feature_produced_in_combo_to_attributes[i.featureproducedincombo_id].add(i.featureattribute_id)
+            x = self.feature_produced_in_combo_to_attributes.get(i.featureproducedincombo_id)
+            if x is not None:
+                x.add(i.featureattribute_id)
         self.card_to_features = {c.id: list[FeatureOfCard]() for c in cards}
         self.features_to_cards = {f.id: list[FeatureOfCard]() for f in features}
         for i in featureofcards:
-            self.card_to_features[i.card_id].append(i)
-            self.features_to_cards[i.feature_id].append(i)
+            x = self.card_to_features.get(i.card_id)
+            y = self.features_to_cards.get(i.feature_id)
+            if x is not None and y is not None:
+                x.append(i)
+                y.append(i)
         self.feature_of_card_to_attributes = {f.id: set[int]() for f in featureofcards}
         for i in featureofcard_attributes:
-            self.feature_of_card_to_attributes[i.featureofcard_id].add(i.featureattribute_id)
+            x = self.feature_of_card_to_attributes.get(i.featureofcard_id)
+            if x is not None:
+                x.add(i.featureattribute_id)
 
         self.variant_to_cards = {v.id: set[CardInVariant]() for v in variants}
         for i in cardinvariants:
-            self.variant_to_cards[i.variant_id].add(i)
-        self.variant_uses_card_dict = {(c.card_id, c.variant_id): c for c in cardinvariants}
+            x = self.variant_to_cards.get(i.variant_id)
+            if x is not None:
+                x.add(i)
+        self.variant_uses_card_dict = {(c.card_id, c.variant_id): c for c in cardinvariants if c.card_id in self.id_to_card and c.variant_id in self.id_to_variant}
 
         self.variant_to_templates = {v.id: set[TemplateInVariant]() for v in variants}
         for i in templateinvariants:
-            self.variant_to_templates[i.variant_id].add(i)
-        self.variant_requires_template_dict = {(t.template_id, t.variant_id): t for t in templateinvariants}
+            x = self.variant_to_templates.get(i.variant_id)
+            if x is not None:
+                x.add(i)
+        self.variant_requires_template_dict = {(t.template_id, t.variant_id): t for t in templateinvariants if t.template_id in self.id_to_template and t.variant_id in self.id_to_variant}
 
         self.variant_to_of_sets = {v.id: set[VariantOfCombo]() for v in variants}
         for i in variantofcombos:
-            self.variant_to_of_sets[i.variant_id].add(i)
-        self.variant_of_combo_dict = {(v.combo_id, v.variant_id): v for v in variantofcombos}
+            x = self.variant_to_of_sets.get(i.variant_id)
+            if x is not None:
+                x.add(i)
+        self.variant_of_combo_dict = {(v.combo_id, v.variant_id): v for v in variantofcombos if v.combo_id in self.id_to_combo and v.variant_id in self.id_to_variant}
 
         self.variant_to_includes_sets = {v.id: set[VariantIncludesCombo]() for v in variants}
         for i in variantincludescombos:
-            self.variant_to_includes_sets[i.variant_id].add(i)
-        self.variant_includes_combo_dict = {(v.combo_id, v.variant_id): v for v in variantincludescombos}
+            x = self.variant_to_includes_sets.get(i.variant_id)
+            if x is not None:
+                x.add(i)
+        self.variant_includes_combo_dict = {(v.combo_id, v.variant_id): v for v in variantincludescombos if v.combo_id in self.id_to_combo and v.variant_id in self.id_to_variant}
 
         self.variant_to_produces = {v.id: set[FeatureProducedByVariant]() for v in variants}
         for i in featureproducedbyvariants:
-            self.variant_to_produces[i.variant_id].add(i)
-        self.variant_produces_feature_dict = {(f.feature_id, f.variant_id): f for f in featureproducedbyvariants}
+            x = self.variant_to_produces.get(i.variant_id)
+            if x is not None:
+                x.add(i)
+        self.variant_produces_feature_dict = {(f.feature_id, f.variant_id): f for f in featureproducedbyvariants if f.feature_id in self.id_to_feature and f.variant_id in self.id_to_variant}
 
         def fetch_not_working_variants(variants: Iterable[Variant]) -> VariantSet:
             variants = [v for v in variants if v.status == Variant.Status.NOT_WORKING]

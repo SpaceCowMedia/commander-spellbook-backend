a9fad5fcca331f6ec10c533ed6ae28433c20a784
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2025-09-18 10:41:47 +0200

    Fix feature status filter not being multiple choice

--- backend/spellbook/views/features.py
+++ backend/spellbook/views/features.py
@@ -1,12 +1,20 @@
 from rest_framework import viewsets
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet, MultipleChoiceFilter
 from spellbook.models import Feature
 from spellbook.serializers import FeatureSerializer
 from .filters import NameAndDescriptionAutocompleteQueryFilter
+
+
+class FeatureFilterSet(FilterSet):
+    status = MultipleChoiceFilter(
+        field_name='status',
+        choices=Feature.Status.choices,
+        conjoined=False,
+    )
 
 
 class FeatureViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = FeatureSerializer.prefetch_related(Feature.objects.exclude(status=Feature.Status.HIDDEN_UTILITY))
     serializer_class = FeatureSerializer
     filter_backends = [DjangoFilterBackend, NameAndDescriptionAutocompleteQueryFilter]
-    filterset_fields = ['status']
+    filterset_class = FeatureFilterSet

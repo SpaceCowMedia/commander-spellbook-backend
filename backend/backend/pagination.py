ee20412a5802d8ee953f71bc7a51bf7aa88ad078
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2025-12-17 00:56:35 +0100

    Set count to null instead of omitting it in the response json

--- backend/backend/pagination.py
+++ backend/backend/pagination.py
@@ -12,8 +12,9 @@ class CustomPagination(LimitOffsetPagination):
     count_query_param = 'count'
     count_query_description = _('Set to true to enable count query. If false, count will be omitted.')
     no_count_template = 'rest_framework/pagination/previous_and_next.html'
+    count = None
 
-    def paginate_queryset(self, queryset, request, view=None):
+    def paginate_queryset(self, queryset, request: Request, view=None):
         self.request = request
         self.count_query = self.get_count_query(request)
         if self.count_query:
@@ -23,8 +24,9 @@ class CustomPagination(LimitOffsetPagination):
             return None
         self.offset = self.get_offset(request)
         result = list(queryset[self.offset:self.offset + self.limit])  # type: ignore
-        self.count = len(result)
-        if (self.count >= self.limit or self.offset > 0) and self.no_count_template is not None:  # type: ignore
+        self.has_next = len(result) >= self.limit  # type: ignore
+        self.has_prev = self.offset > 0
+        if (self.has_next or self.has_prev) and self.no_count_template is not None:  # type: ignore
             self.display_page_controls = True
         return result
 
@@ -37,7 +39,7 @@ class CustomPagination(LimitOffsetPagination):
     def get_next_link(self):
         if self.count_query:
             return super().get_next_link()
-        if self.count < self.limit:  # type: ignore
+        if not self.has_next:  # type: ignore
             return None
         url = self.request.build_absolute_uri()
         url = replace_query_param(url, self.limit_query_param, self.limit)
@@ -64,3 +66,8 @@ class CustomPagination(LimitOffsetPagination):
             },
         })
         return parameters
+
+    def get_paginated_response_schema(self, schema):
+        schema = super().get_paginated_response_schema(schema)
+        schema['properties']['count']['nullable'] = True
+        return schema

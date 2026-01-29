525b90b2110411f94f988f024a81234710089d12
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2026-01-29 10:15:33 +0100

    Raise priority of combo of the day to avoid delays

--- backend/spellbook/tasks/combo_of_the_day.py
+++ backend/spellbook/tasks/combo_of_the_day.py
@@ -9,7 +9,7 @@ from .discord_webhook import discord_webhook
 logger = logging.getLogger(__name__)
 
 
-@task
+@task(priority=10)
 def combo_of_the_day_task():
     '''Replaces the combo of the day with a random public variant different from the current one.'''
     logger.info('Replacing the combo of the day...')
@@ -27,7 +27,7 @@ def combo_of_the_day_task():
     announcement = None
     if new_combo:
         website_property.value = str(new_combo.pk)
-        logger.info(f'Combo of the day has been {"replaced with" if current_combo else "set to"} {new_combo.pk}: {new_combo.name}')
+        logger.info(f'Combo of the day has been {'replaced with' if current_combo else 'set to'} {new_combo.pk}: {new_combo.name}')
         announcement = f'# ♾️ New Combo of the Day! ♾️\n\n[{new_combo.name}]({new_combo.spellbook_link(raw=True)})'
     website_property.save()
     if announcement:

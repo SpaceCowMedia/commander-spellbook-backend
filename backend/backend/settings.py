0b2f993e79327b21e91667804c63e69fc1ebc6c0
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2026-03-09 14:13:41 +0100

    Fix issue with nullable enums and spectacular default setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -247,6 +247,7 @@ SPECTACULAR_SETTINGS = {
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
     'REDOC_DIST': 'SIDECAR',
     'COMPONENT_SPLIT_REQUEST': True,
+    'ENUM_ADD_EXPLICIT_BLANK_NULL_CHOICE': False,
     'POSTPROCESSING_HOOKS': [
         'drf_spectacular.hooks.postprocess_schema_enums',
         'drf_spectacular.contrib.djangorestframework_camel_case.camelize_serializer_fields',
